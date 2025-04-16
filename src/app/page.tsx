2daf8eef5aca3947da79aef2714342ad3bab3cfa
alexgt95 <ale.g.trujillo2005@gmail.com>
2025-04-16 14:04:52 -0600

    Customization 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Progress } from "@radix-ui/react-progress";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
         <div>
-          <Button>I am a button</Button>
+          <Button variant="elevated">I am a button</Button>
         </div>
         <div>
           <Input placeholder="I am an input" />
         </div>
         <div>
-          <Progress value={50} />
+          <Progress value={80} />
         </div>
         <div>
           <Textarea placeholder="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox />
         </div>
       </div>
     </div>
